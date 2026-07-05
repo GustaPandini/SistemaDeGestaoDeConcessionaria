b442d2b0813967387580958aeadeeb775582c15d
GustaPandini <gugui.pandini@gmail.com>
2026-07-05 14:06:00 -0300

    feat: Organizado a estrutura de arquitetura limpa com separação de funções criando e referenciando os projetos em camadas (.Domain, .Infra.Data, .Infra.Ioc e .Application)
    feat: Adicionado o Entitie Usuario

---
+++ Back-end/SistemaGestaoDeConcessionaria/SistemaGestaoDeConcessionaria.Domain/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaGestaoDeConcessionaria.Domain.Entities
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Perfil { get; set; }
+    }
+}
