95847ba051f7389346283f653d268ec8ebc2a503
GustaPandini <gugui.pandini@gmail.com>
2026-07-06 20:52:26 -0300

    feat: Criado a Injeção de dependencia das classes usando o MySql.EntitieFrameWork

---
+++ Back-end/SistemaGestaoDeConcessionaria/SistemaDeGestaoDeConcessionaria.Infra.Ioc/DependecyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaDeGestaoDeConcessionaria.Infra.Data.Context;
+using SistemaDeGestaoDeConcessionaria.Infra.Data.Repositorys;
+using SistemaGestaoDeConcessionaria.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaDeGestaoDeConcessionaria.Infra.Ioc
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseMySQL(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+                  
+            });
+
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IAutomovelRepository, AutomovelRepository>();
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<IVendaRepository, VendaRepository>();
+
+            return services;
+
+            
+        }
+    }
+}
