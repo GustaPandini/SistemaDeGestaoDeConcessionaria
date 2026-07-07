678a2c06169425dcec898485a16dded9e4eb8282
GustaPandini <gugui.pandini@gmail.com>
2026-07-06 21:31:54 -0300

    feat: Feito a initialMigration do banco

---
+++ Back-end/SistemaGestaoDeConcessionaria/SistemaDeGestaoDeConcessionaria.Infra.Data/Migrations/20260707002242_initialMigration.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SistemaDeGestaoDeConcessionaria.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Automovel",
+                columns: table => new
+                {
+                    idAutomovel = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    PlacaOuChassi = table.Column<string>(type: "varchar(17)", maxLength: 17, nullable: false),
+                    Marca = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Modelo = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Powertrain = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Versao = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Cor = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    AnoModelo = table.Column<int>(type: "int", nullable: false),
+                    Quilometragem = table.Column<int>(type: "int", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Blindado = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    QuantidadeDonos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Automovel", x => x.idAutomovel);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    idCliente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    CPF = table.Column<int>(type: "int", maxLength: 11, nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    Endereco = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.idCliente);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    idUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "longblob", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "longblob", nullable: false),
+                    Perfil = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.idUsuario);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Venda",
+                columns: table => new
+                {
+                    idVenda = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    DataDaVenda = table.Column<DateOnly>(type: "date", nullable: false),
+                    ValorPago = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FormaDePagamento = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    idAutomovel = table.Column<int>(type: "int", nullable: false),
+                    idCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venda", x => x.idVenda);
+                    table.ForeignKey(
+                        name: "FK_Venda_Automovel_idAutomovel",
+                        column: x => x.idAutomovel,
+                        principalTable: "Automovel",
+                        principalColumn: "idAutomovel");
+                    table.ForeignKey(
+                        name: "FK_Venda_Cliente_idCliente",
+                        column: x => x.idCliente,
+                        principalTable: "Cliente",
+                        principalColumn: "idCliente");
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Automovel_PlacaOuChassi",
+                table: "Automovel",
+                column: "PlacaOuChassi",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_Email",
+                table: "Usuario",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venda_idAutomovel",
+                table: "Venda",
+                column: "idAutomovel");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venda_idCliente",
+                table: "Venda",
+                column: "idCliente");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Venda");
+
+            migrationBuilder.DropTable(
+                name: "Automovel");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
